ba0cf869d6903271fefd2c6d42e223267a727325
Rahul <rahulsmrd@gmail.com>
2023-07-08 09:29:25 +0530

    blog

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
-        'USER': 'Rahul2',
-        'PASSWORD': 'Rahul2003',
-        'HOST': 'blog.cyifipeqlrw2.eu-north-1.rds.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
